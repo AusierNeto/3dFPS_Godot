e9628227c56d6fd72b320162a9fc1e5a767bab0d
AusierNeto <ausierneto@gmail.com>
2025-12-28 20:05:08 -0400

    Feat: Add recoil animation

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,34 +4,41 @@ const MOUSE_SENSITIVITY:float = 0.002
 const SPEED:float = 5.0
 const JUMP_VELOCITY:float = 4.5
 
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+# --- Animation ---
+const SWAY_AMOUNT = 0.05       # O quanto a arma balança (Amplitude)
+const SWAY_FREQ = 10.0         # Velocidade do balanço (Frequência)
+const RECOIL_AMOUNT = 0.3      # O quanto a arma vai pra trás no tiro
+const RECOIL_RETURN_SPEED = 5.0 # Quão rápido ela volta ao normal
 
 @onready var head = $Head
+@onready var arms = $Head/Arms
 @onready var ray_cast = $Head/Camera3D/RayCast3D
 @onready var gun_sound = $Head/GunShot
 @onready var gun_particles = $Head/MuzzleFlash
 
+var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+var default_hand_position: Vector3
+
 func _ready() -> void:
 	# Faz com que o mouse fique preso à camera
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	print("Arms position", arms.position)
+	default_hand_position = arms.position
 	
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		rotate_y(-event.relative.x * MOUSE_SENSITIVITY)
 		head.rotate_x(-event.relative.y * MOUSE_SENSITIVITY)
-		
 		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-60), deg_to_rad(90))
 
 func _physics_process(delta: float) -> void:
+	# Jumping and vertical movement
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-		
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		
-	if Input.is_action_just_pressed("shoot"):
-		fire_weapon()
-		
+	
+	# WASD Movement
 	var input_direction = Input.get_vector("move_left", "move_right", "move_forward", "move_backwards")
 	var normalized_direction = (transform.basis * Vector3(input_direction.x, 0, input_direction.y))
 
@@ -41,14 +48,28 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-	
 	move_and_slide()
+	
+	if Input.is_action_just_pressed("shoot"):
+		fire_weapon()
+	process_weapon_animation(delta, input_direction)
+	
+func process_weapon_animation(delta, input_direction):
+	# Parte A: Recoil Recovery (Voltar ao normal)
+	# Usamos lerp para mover da posição atual (que pode estar recuada) para a original
+	arms.position = arms.position.lerp(default_hand_position, delta * RECOIL_RETURN_SPEED)
+	
+	# Head Bobbing
 	
 func fire_weapon():
 	gun_sound.play()
 	
 	gun_particles.restart()
 	gun_particles.emitting=true
+	
+	# recoil animation
+	arms.position.z += 0.2
+	#arms.rotation.x += 0.1
 	
 	if ray_cast.is_colliding():
 		var target = ray_cast.get_collider()
